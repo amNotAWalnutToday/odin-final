c2d918e9d6c9f715b30ae2d51d796d562c0b0910
unknown <117041148+amNotAWalnutToday@users.noreply.github.com>
2023-04-20 03:39:53 +0100

    Add template header

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+export default function Header() {
+    return (
+        <header className="header" >
+            <h1>Replicatedit</h1>
+            <input type="search" />
+            <button>Login</button>
+            <button>ø</button>
+        </header>
+    )
+}
